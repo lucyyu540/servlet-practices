6600180776835590b701da5c678c7669f3b4d28c
Lucy Yu <lucyyu540@gmail.com>
2021-03-09 13:01:57 +0900

    httpservlet helloooo  on html

---
+++ hello-web/src/main/java/com/lucy/controller/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.lucy.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("<h1> helloooooo</h1>");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
